787b98a78439caf6b94ff1bc758798a72098fb65
Lucy Block <lucyblock@iphone.vl40.pratt.edu>
2017-09-05 21:22:00 -0400

    update

---
+++ lbscript.js
@@ -0,0 +1,11 @@
+var activities = ["walk to the left", "walk to the right",
+ "walk forward", "walk backward"]
+
+var steps = ["first", "second", "third", "fourth"]
+
+ for (var i = 0; i < activities.length; i++) {
+  activities[i];
+
+    console.log("The", steps[i], "thing I do in the morning is", activities[i]);
+
+ }
